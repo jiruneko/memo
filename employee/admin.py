90cd9deb9dd04bd5a7aac26931870578ab4a7362
jiruneko <oohikaru310@gmail.com>
2022-02-06 12:26:45 +0900

    first commit

---
+++ employee/admin.py
@@ -0,0 +1,7 @@
+from atexit import register
+from django.contrib import admin
+from .models import Department, Employee, Club
+
+admin.site.register(Department)
+admin.site.register(Employee)
+admin.site.register(Club)
